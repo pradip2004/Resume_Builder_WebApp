01590af893745d6865ef5b40b6b4d13d00eb6de1
pradip2004 <pradipsadhukhan16@gmail.com>
2025-03-01 01:14:25 +0530

    Update: Frontend

--- MERN-2/src/components/custom/FormSection.jsx
+++ MERN-2/src/components/custom/FormSection.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import PersonalDetails from './FormSection/PersonalDetails'
+import { Button } from '../ui/button'
+import { ArrowLeft, ArrowRight, LayoutGrid } from 'lucide-react'
 
 function FormSection() {
+
+  const [activeFormIndex, setActiveFormIndex] = useState(1);
+  const [enableNext, setEnableNext] = useState(false)
+
+
   return (
-    <div>FormSection</div>
+    <div>
+      <div className='flex justify-between items-center'>
+        <Button variant="outline" size="sm" className="flex gap-2"><LayoutGrid />Theme</Button>
+        <div className='flex gap-2'>
+          {activeFormIndex > 1
+            && <Button onClick={()=>setActiveFormIndex(activeFormIndex-1)} size="sm">
+              <ArrowLeft />
+            </Button>}
+          <Button className="flex gap-2" size="sm"
+            onClick={()=>setActiveFormIndex(activeFormIndex+1)}
+            disabled={!enableNext}
+          >
+            Next <ArrowRight />
+          </Button>
+        </div>
+      </div>
+      {/* personal details  */}
+      {activeFormIndex == 1 ? <PersonalDetails enableNext={(v)=>setEnableNext(v)}/> : null}
+      {/* summary  */}
+
+      {/* Experience  */}
+
+      {/* Educational Details  */}
+
+      {/* Skills  */}
+    </div>
   )
 }
 
---
+++ MERN-2/src/components/custom/FormSection/PersonalDetails.jsx
@@ -0,0 +1,79 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { ResumeContextInfo } from '@/context/ResumeContextInfo'
+import { LoaderCircle } from 'lucide-react'
+import React, { useContext } from 'react'
+
+function PersonalDetails({enableNext}) {
+  const {resumeInfo, setResumeInfo} = useContext(ResumeContextInfo)
+
+  const handleInputChange = (e) => {
+    enableNext(false)
+    const {name, value} = e.target;
+
+    setResumeInfo({
+      ...resumeInfo,
+      [name]: value
+    })
+  }
+
+  const onSave = (e) => {
+    e.preventDefault()
+    enableNext(true);
+
+  }
+
+  return (
+    <div className='p-5 shadow-lg rounded-lg border-t-primary border-t-4 mt-10'>
+         <h2 className='font-bold text-lg'>Personal Detail</h2>
+         <p>Get Started with the basic information</p>
+
+         <form onSubmit={onSave}>
+            <div className='grid grid-cols-2 mt-5 gap-3'>
+                <div>
+                    <label className='text-sm'>First Name</label>
+                    <Input name="firstName" defaultValue={resumeInfo?.firstName} required onChange={handleInputChange}  />
+                </div>
+                <div>
+                    <label className='text-sm'>Last Name</label>
+                    <Input name="lastName" required onChange={handleInputChange} 
+                    defaultValue={resumeInfo?.lastName} />
+                </div>
+                <div className='col-span-2'>
+                    <label className='text-sm'>Job Title</label>
+                    <Input name="jobTitle" required 
+                    defaultValue={resumeInfo?.jobTitle}
+                    onChange={handleInputChange}  />
+                </div>
+                <div className='col-span-2'>
+                    <label className='text-sm'>Address</label>
+                    <Input name="address" required 
+                    defaultValue={resumeInfo?.address}
+                    onChange={handleInputChange}  />
+                </div>
+                <div>
+                    <label className='text-sm'>Phone</label>
+                    <Input name="phone" required 
+                    defaultValue={resumeInfo?.phone}
+                    onChange={handleInputChange}  />
+                </div>
+                <div>
+                    <label className='text-sm'>Email</label>
+                    <Input name="email" required 
+                    defaultValue={resumeInfo?.email}
+                    onChange={handleInputChange}  />
+                </div>
+            </div>
+            <div className='mt-3 flex justify-end'>
+                <Button type="submit"
+                >
+                    {/* {loading?<LoaderCircle className='animate-spin' />:'Save'} */}
+                    Save
+                    </Button>
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default PersonalDetails
