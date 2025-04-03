56ae466e294c7abc53e0bdd8dfe672aa55dc6720
pradip2004 <pradipsadhukhan16@gmail.com>
2025-04-03 14:34:23 +0530

    Update Auth

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ const app = express()
 
 // Configure CORS with specific options
 app.use(cors({
-  origin: [process.env.FRONTEND_URL, 'http://localhost:5173'],
+  origin: [process.env.FRONTEND_URL, 'http://localhost:5173', 'https://resumegpt-psi.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
